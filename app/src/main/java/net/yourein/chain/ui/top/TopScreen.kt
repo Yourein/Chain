6e33157d06cadf3c4bbfb7563d188a0006c86316
Yourein <rein.you.3.1@gmail.com>
2024-12-18 23:05:55 +0900

    TopScreenにロゴが出るようにした

--- app/src/main/java/net/yourein/chain/ui/top/TopScreen.kt
+++ app/src/main/java/net/yourein/chain/ui/top/TopScreen.kt
@@ -1,16 +1,24 @@
 package net.yourein.chain.ui.top
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.yourein.chain.R
 import net.yourein.chain.ui.common.ChainTitleText
 import net.yourein.chain.ui.common.ChainTopBar
 import net.yourein.chain.ui.theme.ChainTheme
@@ -35,13 +43,26 @@ private fun TopScreen(
         contentWindowInsets = WindowInsets(0)
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
         ) {
             TopScreenTabRow(
                 selectedTab = selectedTab,
                 unreadCount = unreadCount,
                 onTabClicked = { selectedTab = it },
             )
+
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.shinycolors),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(300.dp)
+                        .align(Alignment.TopEnd)
+                )
+            }
         }
     }
 }
