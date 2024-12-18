a99eb0f0bc10e29f061926112697ce3b24b5f344
Yourein <rein.you.3.1@gmail.com>
2024-12-19 01:51:13 +0900

    ユニット順に並べ替え

--- app/src/main/java/net/yourein/chain/ui/top/TopScreenViewModel.kt
+++ app/src/main/java/net/yourein/chain/ui/top/TopScreenViewModel.kt
@@ -17,6 +17,7 @@ class TopScreenViewModel : ViewModel() {
         mockRoom,
         mockRoom.copy(name = "風野灯織", iconRes = R.drawable.chain_icon_hiori),
         mockRoom.copy(name = "めぐる", iconRes = R.drawable.chain_icon_meguru),
+        mockRoom.copy(name = "恋鐘", iconRes = R.drawable.chain_icon_kogane),
         mockRoom.copy(name = "まみみ", iconRes = R.drawable.chain_icon_mamimi),
         mockRoom.copy(
             name = "白瀬咲耶 / Sakuya Shirase",
@@ -41,12 +42,18 @@ class TopScreenViewModel : ViewModel() {
             name = "Natsuha Arisugawa",
             iconRes = R.drawable.chain_icon_natsuha
         ),
+        mockRoom.copy(name = "大崎甘奈", iconRes = R.drawable.chain_icon_amana),
         mockRoom.copy(name = "てんか", iconRes = R.drawable.chain_icon_tenka),
         mockRoom.copy(
             name = "桑山千雪",
             iconRes = R.drawable.chain_icon_chiyuki
         ),
         mockRoom.copy(name = "ふゆ", iconRes = R.drawable.chain_icon_fuyuko),
+        mockRoom.copy(
+            name = "芹沢あさひ",
+            iconRes = R.drawable.chain_icon_asahi
+        ),
+        mockRoom.copy(name = "愛依", iconRes = R.drawable.chain_icon_mei),
         mockRoom.copy(name = "浅倉", iconRes = R.drawable.chain_icon_toru),
         mockRoom.copy(name = "樋口", iconRes = R.drawable.chain_icon_madoka),
         mockRoom.copy(
@@ -58,18 +65,11 @@ class TopScreenViewModel : ViewModel() {
             name = "緋田美琴",
             iconRes = R.drawable.chain_icon_mikoto
         ),
-        mockRoom.copy(name = "羽那", iconRes = R.drawable.chain_icon_hana),
-        mockRoom.copy(name = "恋鐘", iconRes = R.drawable.chain_icon_kogane),
-        mockRoom.copy(name = "大崎甘奈", iconRes = R.drawable.chain_icon_amana),
-        mockRoom.copy(
-            name = "芹沢あさひ",
-            iconRes = R.drawable.chain_icon_asahi
-        ),
-        mockRoom.copy(name = "愛依", iconRes = R.drawable.chain_icon_mei),
         mockRoom.copy(
             name = "七草にちか",
             iconRes = R.drawable.chain_icon_nichika
         ),
+        mockRoom.copy(name = "羽那", iconRes = R.drawable.chain_icon_hana),
         mockRoom.copy(name = "いかるが", iconRes = R.drawable.chain_icon_luca),
         mockRoom.copy(
             name = "いくはる",
